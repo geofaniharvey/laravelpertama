220c9b8e97fd89358bb160d01268217f77c80b62
geofaniharvey <72368521+geofaniharvey@users.noreply.github.com>
2021-01-20 11:34:19 +0700

    commit 8 REST API GET POST

---
+++ app/Http/Controllers/Api/CobaController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\Friends;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CobaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $friends = Friends::orderby('id', 'desc')->paginate(3);
+
+        return response()->json([
+            'success'   => true,
+            'message'   => 'Daftar Data Teman',
+            'data'  => $friends
+        ], 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nama' => 'required|unique:friends|max:255|',
+            'no_telp' => 'required|numeric',
+            'alamat' => 'required'
+        ]);
+
+        $friends = Friends::create([
+            'nama'  => $request->nama,
+            'no_telp'   => $request->no_telp,
+            'alamat'    => $request->alamat
+            
+        ]);
+
+            if($friends)
+            {
+                return response()->json([
+                    'success'   => true,
+                    'message'   => 'Berhasil Menambahkan Teman',
+                    'data'  => $friends
+                ], 200);
+            }else{
+                return response()->json([
+                    'success'   => false,
+                    'message'   => 'Gagal Menambahkan Teman',
+                    'data'  => $friends
+                ], 409);
+            }
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
